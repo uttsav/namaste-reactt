6d7e584a6138471f98804020e345a7c1daef44eb
Utsav <utsavoza2710@gmail.com>
2024-09-14 22:31:13 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h2", {}, "This is h2 inside a div"),
+    React.createElement("h1", {}, "This is h1 inside a div"),
     React.createElement(
       "h2",
       {},
@@ -8,11 +11,11 @@ const parent = React.createElement("div", { id: "parent" }, [
     ),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h2", {}, "This is h2 inside a div"),
+    React.createElement("h3", {}, "This is h3 inside a div"),
     React.createElement(
-      "h2",
+      "h4",
       {},
-      "This is h2 inside a div but this is sibling"
+      "This is h4 inside a div but this is sibling child"
     ),
   ]),
 ]);
---
+++ README.md
@@ -0,0 +1,15 @@
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR Hot Module Replacement
+- Caching Faster Builds
+- Image Optimization
+- file minification
+- file bundling
+- consistent hashing
+- code splitting
+- differential bundling -> for support for older browser
+- error handling
+- Tree shaking - remove unused code
+- different dev and production bundles
